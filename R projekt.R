6188e6e377d765b627ae488cd4bfec676013a3d6
Dzidu <roznowski97@wp.pl>
2023-06-11 02:01:06 +0200

    Analiza tematu

--- R projekt.R
+++ R projekt.R
@@ -6,6 +6,14 @@ library(hunspell)
 library(rvest)
 library(stringr)
 library(RSelenium)
+library(wordcloud)
+library(wordcloud2)
+library(plotrix)
+library(lubridate)
+library(topicmodels)
+library(reshape2)
+library(textdata)
+
 
 base_url_pt1 <- "https://cryptonews.net/?page="
 
@@ -57,7 +65,7 @@ get_article <- function(art_url) {
   
   page <- page$result
   
-  author <- page %>% html_node('span.source-host') %>% html_text() %>% trimws()
+  #author <- page %>% html_node('span.source-host') %>% html_text() %>% trimws()
   
   date <- page %>% html_node("span.datetime.flex.middle-xs") %>% html_text() %>% trimws() %>% dmy_hm()
   
@@ -68,7 +76,7 @@ get_article <- function(art_url) {
   body <- page %>% html_node("div.news-item.detail.content_text") %>% html_nodes("p") %>% html_text() %>% trimws()
   
   
-  article <- tibble(url = art_url, title = title, author = author, date = date, lead = lead, body = body,)
+  article <- tibble(url = art_url, title = title, date = date, lead = lead, body = body,)
   
   Sys.sleep(sample(seq(0.25, 1, 0.5), 1))
   
@@ -82,7 +90,7 @@ articles <- article_links %>%
   ungroup()
 
 articles_grouped <- articles %>%
-  group_by(url,title,author,date,lead )  %>%
+  group_by(url,title,date,lead )  %>%
   summarise(full_body = paste(body, collapse = " "))
 
 #getwd()
@@ -90,8 +98,6 @@ articles_grouped <- articles %>%
 #saveRDS(articles_grouped, file = "articles_full.RDS")
 
 
-articles_long <- articles_grouped["full_body"] %>% 
-  unnest_tokens(word,full_body)
 #  anti_join(stop_words, by = 'word') %>%
 #  filter(!str_detect(word, '[0-9]')) %>%
 #  filter(str_detect(word, '[A-z]')) %>%
@@ -105,19 +111,85 @@ articles_long <- articles_grouped["full_body"] %>%
 #articles_long <- articles_long %>% 
 #  mutate(word3 = ifelse(is.na(word2), word, word2))
 
-articlesCorpus <- Corpus(VectorSource(articles_long))
+
 clean.corpus <- function(corpus){
   corpus <- tm_map(corpus, tolower) 
   corpus <- tm_map(corpus, removeWords, stopwords('en')) 
   corpus <- tm_map(corpus, removePunctuation)
   corpus <- tm_map(corpus, stripWhitespace) 
   corpus <- tm_map(corpus, removeNumbers)
-  corpus <- tm_map(corpus, stemDocument)
+  corpus <- tm_map(corpus, content_transformer(lemmatize_strings))
   return(corpus)
 }
-articlesCorpus <-  clean.corpus(articlesCorpus)
+articles_grouped$full_body_lemmatized <- NA
+for(i in 1:nrow(articles_grouped)) {
+  articles_long <- articles_grouped[i,"full_body"] %>% 
+    unnest_tokens(word,full_body )
+  articlesCorpus <- Corpus(VectorSource(articles_long))
+  articlesCorpus <- clean.corpus(articlesCorpus)
+  articles_grouped[i,"full_body_lemmatized"] <- data.frame(text = sapply(articlesCorpus, as.character), stringsAsFactors = FALSE)
+}
 
-inspect(articlesCorpus[1])
+word_tokens <- articles_grouped %>%
+  unnest_tokens(word, full_body_lemmatized, drop = FALSE) %>%
+  select(-full_body_lemmatized) %>% 
+  filter(!str_detect(word, '\\d')) %>%
+  filter(nchar(word) > 2)
+
+#chmura słow
+word_count <- word_tokens %>% 
+  count(word)
+par(mar = c(0, 0, 0, 0))
+wordcloud(word_count$word, 
+          word_count$n, max.words = 100, 
+          rot.per = 0.35, colors = brewer.pal(12,"Paired"))
+
+#top n slow
+
+word_tokens$month <- month(ymd_hms(word_tokens$date))
+
+word_top <- word_tokens[,-1:-3] %>% 
+  count(month, word, sort = T) %>% 
+  group_by(month) %>% 
+  top_n(15, n) %>% 
+  ungroup() %>% 
+  mutate(word = reorder(word, n))
+
+ggplot(word_top, aes(x = reorder_within(word, n, month), y = n, fill = month)) +
+  geom_col(show.legend = F) +
+  coord_flip() +
+  facet_wrap(~month, scales ="free_y" ) +
+  scale_x_reordered()
+
+
+#top 3 tematy
+word_tokens_long<-word_tokens[,-c(1,2,3,5)]
+word_tokens_long_corpus <- Corpus(VectorSource(word_tokens_long$word))
+
+dtm <- DocumentTermMatrix(word_tokens_long_corpus,
+                          control = list(weighting = weightTf))
+
+topic_minutes <- LDA(dtm, 
+                     k = 4,
+                     control = list(seed = 2137692115))
+
+mi_topics <- tidy(topic_minutes, matrix = "beta") 
+
+top_terms <- mi_topics %>%
+  group_by(topic) %>% # grupujemy wg tematów
+  top_n(10, beta) %>% # 10 największych prawdopodobieństw beta w każdym temacie
+  ungroup() %>%
+  arrange(topic, -beta) # ustalamy wg największych wartości beta
+
+top_terms %>%
+  mutate(term = reorder_within(term, beta, topic)) %>% # układa wyrażenia wg malejących wartości beta
+  ggplot(aes(term, beta, fill = factor(topic))) + # tematy będą zaznaczone innymi kolorami
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +
+  coord_flip()+
+  scale_x_reordered()
+
+#analiza emocji
 
 
 
